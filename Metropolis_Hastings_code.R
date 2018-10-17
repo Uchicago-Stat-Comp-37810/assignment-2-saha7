4f0ea73622cc75b96a413ae4218d65e1df68a7e8
Seungah Ha <saha@uchicago.edu>
2018-10-17 15:50:25 -0500

    R#3
    R: Problem3
    Modifying the summary part to a function.

--- Metropolis_Hastings_code.R
+++ Metropolis_Hastings_code.R
@@ -80,22 +80,23 @@ acceptance=1-mean(duplicated(chain[-(1:burnIn),]))
 
 # Summary: Here, we draw 6 plots that show the posterior of parameters and how chain values have changed. 
 
-par(mfrow = c(2,3))
-hist(chain[-(1:burnIn),1], nclass=30, main="Posterior of a", xlab="True value = red line" )
-abline(v = mean(chain[-(1:burnIn),1]))
-abline(v = trueA, col="red" )
-hist(chain[-(1:burnIn),2],nclass=30, main="Posterior of b", xlab="True value = red line")
-abline(v = mean(chain[-(1:burnIn),2]))
-abline(v = trueB, col="red" )
-hist(chain[-(1:burnIn),3],nclass=30, main="Posterior of sd", xlab="True value = red line")
-abline(v = mean(chain[-(1:burnIn),3]) )
-abline(v = trueSd, col="red" )
-plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of a")
-abline(h = trueA, col="red" )
-plot(chain[-(1:burnIn),2], type = "l", xlab="True value = red line" , main = "Chain values of b")
-abline(h = trueB, col="red" )
-plot(chain[-(1:burnIn),3], type = "l", xlab="True value = red line" , main = "Chain values of sd")
-abline(h = trueSd, col="red" )
+true <- c(5,0,10)
+
+summaryplot <- function(chain, burnIn, truevec){
+  par(mfrow = c(2,3))
+  param <- c("a","b","sd")
+  
+  for(i in 1:3){
+    hist(chain[-(1:burnIn),i], nclass=30, main=paste0("Posterior of ",param[i]), xlab="True value = red line" )
+    abline(v = mean(chain[-(1:burnIn),i]))
+    abline(v = true[i], col="red")
+  }
+  
+  for(i in 1:3){
+    plot(chain[-(1:burnIn),i], type = "l", xlab="True value = red line" , main = paste0("Chain values of ",param[i]))
+    abline(h = true[i], col="red" )
+  }
+}
 
 # for comparison:
 summary(lm(y~x))
